e57bf762f8acca4723d18473e012c739a2982e3f
james-crisp <97354178+james-crisp@users.noreply.github.com>
2022-02-05 18:42:22 -0500

    Adding file
    Added first file for test

---
+++ first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 18:38:25 2022
+
+@author: jimmycrisp
+"""
+
+import numpy as np
+
+def recordCleaning(prices):
+    weights = 1
+    biases = 1
+    point1 = np.zeros(16)
+    point2 = np.zeros(16)
+    print(point1)
+    return
+
+recordCleaning(1)
